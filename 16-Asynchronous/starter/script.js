e4cb29e6677e0804c2d4647827626bc8773ba81a
Johan Lind <johan.lind.se@gmail.com>
2023-09-22 13:06:59 +0200

    Nested callbacks, callback-hell.

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -12,7 +12,8 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 // Old school http requests
-// Good to know, you might encounter xthis in older apps
+// Good to know, you might encounter this in older apps
+/* 
 const getCountryData = function (country) {
 const request = new XMLHttpRequest();
 request.open('GET', `https://restcountries.com/v2/name/${country}`);
@@ -41,4 +42,71 @@ request.addEventListener('load', function(){
 getCountryData('sweden');
 getCountryData('portugal');
 getCountryData('usa');
+ */
+///
+// Nested callbacks
+const renderCountry = function(data, className = '') {
+    const html = `
+        <article class="country ${className}">
+            <img class="country__img" src="${data.flags.svg}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} mn people</p>
+                <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+                <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+            </div>
+        </article>
+        `;
+    countriesContainer.insertAdjacentHTML('beforeEnd', html);
+    countriesContainer.style.opacity = 1;
+}
 
+const getCountryAndNeighbour = function (country) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v2/name/${country}`);
+    request.send();
+    
+    request.addEventListener('load', function(){
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+        renderCountry(data);
+
+        const [neighbur] = data.borders;
+
+        console.log(neighbur)
+
+        if (!neighbur) return;
+    
+        const request2 = new XMLHttpRequest();
+        request2.open('GET', `https://restcountries.com/v2/alpha/${neighbur}`);
+        request2.send();
+
+        request2.addEventListener('load', function() {
+           const data2 = JSON.parse(request2.responseText);
+           console.log(data2)
+           renderCountry(data2, 'neighbour')
+        })
+
+    });
+}
+    
+getCountryAndNeighbour('sweden');
+
+
+// Easaly becomes like something called "callback hell", trangel formatted code :-)
+setTimeout(() => {
+    console.log('1 second')
+    setTimeout(() => {
+        console.log('2 second')
+        setTimeout(() => {
+            console.log('3 second')
+            setTimeout(() => {
+                console.log('4 second')
+                setTimeout(() => {
+                    console.log('5 second')
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
