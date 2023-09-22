0771381c1af08eae7f867e8ce521beec59ef42c7
Johan Lind <johan.lind.se@gmail.com>
2023-09-22 07:23:09 +0200

    Add comment for api adress change.

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,4 +1,10 @@
 'use strict';
+/* 
+    It's not a big deal, it's really just one small change. Instead of:
+    https://restcountries.eu/rest/v2/
+    It's now:
+    https://countries-api-836d.onrender.com/countries/
+ */
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
